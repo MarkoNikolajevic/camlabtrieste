f6af3c1fdad63a52680aff2b3709181f91bcf210
Marko Nikolajevic <mrknikolajevic@gmail.com>
2018-06-26 15:27:16 +0200

    Responsive navbar

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -6,3 +6,30 @@ document.addEventListener('DOMContentLoaded', function() {
 		headerText.classList.add('fadeIn');
 	}, 1000);
 });
+
+// Responsive navbar
+mobileNav();
+
+
+function mobileNav() {
+	const hamburger = document.querySelector('.hamburger-menu');
+	const nav = document.querySelector('.nav-mobile');
+	hamburger.addEventListener('click', function() {
+		hamburger.classList.toggle('animated');
+		nav.classList.toggle('display-none');
+		closeNav();
+	});
+}
+
+function closeNav() {
+	const hamburger = document.querySelector('.hamburger-menu');
+	const nav = document.querySelector('.nav-mobile');
+	let links = document.querySelectorAll('.nav-mobile-link');
+	links = Array.from(links);
+	links.forEach(function(elem) {
+		elem.addEventListener('click', function() {
+			hamburger.classList.remove('animated');
+			nav.classList.add('display-none');
+		});
+	});
+}
