8c549b5e7e5526b6fb97fb0f77f745a452c14919
Marko Nikolajevic <mrknikolajevic@gmail.com>
2020-03-24 16:50:41 +0100

    Add online course booking

--- gulpfile.js
+++ gulpfile.js
@@ -1,82 +1,96 @@
-const gulp = require("gulp");
-const imagemin = require("gulp-imagemin");
-const webp = require("gulp-webp");
-const sass = require("gulp-sass");
-const uglify = require("gulp-uglify");
-const pump = require("pump");
-const babel = require("gulp-babel");
-const autoprefixer = require("gulp-autoprefixer");
-const sourcemaps = require("gulp-sourcemaps");
-const browserSync = require("browser-sync").create();
-const rename = require("gulp-rename");
-require("babel-register");
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
+const pump = require('pump');
+const babel = require('gulp-babel');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
+const rename = require('gulp-rename');
+require('babel-register');
 
 // Optimize images
-gulp.task("optimizeImg", () =>
-  gulp.src("src/assets/image/*")
-    .pipe(imagemin({
-      progressive: true,
-      verbose: true
-    }))
-    .pipe(gulp.dest("dist/assets/image"))
+gulp.task('optimizeImg', () =>
+  gulp
+    .src('src/assets/image/*')
+    .pipe(
+      imagemin({
+        progressive: true,
+        verbose: true
+      })
+    )
+    .pipe(gulp.dest('dist/assets/image'))
 );
 
 // Convert images into webp format
-gulp.task("webpImg", () =>
-  gulp.src("dist/assets/image/*")
+gulp.task('webpImg', () =>
+  gulp
+    .src('dist/assets/image/*')
     .pipe(webp())
-    .pipe(gulp.dest("dist/assets/image/"))
+    .pipe(gulp.dest('dist/assets/image/'))
 );
 
 // Compile sass and minify
-gulp.task("sass", () =>
-  gulp.src("src/assets/style/sass/**/*.sass")
+gulp.task('sass', () =>
+  gulp
+    .src('src/assets/style/sass/**/*.sass')
     .pipe(sourcemaps.init())
-    .pipe(sass({
-      outputStyle: "compressed"
-    }))
-    .on("error", sass.logError)
-    .pipe(autoprefixer({
-      browsers: "last 5 versions"
-    }))
-    .pipe(rename({
-      suffix: ".min"
-    }))
-    .pipe(sourcemaps.write("./"))
-    .pipe(gulp.dest("dist/assets/style/css"))
+    .pipe(
+      sass({
+        outputStyle: 'compressed'
+      })
+    )
+    .on('error', sass.logError)
+    .pipe(
+      autoprefixer({
+        browsers: 'last 5 versions'
+      })
+    )
+    .pipe(
+      rename({
+        suffix: '.min'
+      })
+    )
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('dist/assets/style/css'))
     .pipe(browserSync.stream())
 );
 
 // Minify js files
-gulp.task("minifyJs", (cb) =>
-  pump([
-    gulp.src("src/assets/js/*.js"),
-    sourcemaps.init(),
-    babel({
-      presets: ["env"]
-    }),
-    uglify(),
-    rename({
-      suffix: ".min"
-    }),
-    sourcemaps.write("."),
-    gulp.dest("dist/assets/js"),
-    browserSync.stream()
-  ], cb)
+gulp.task('minifyJs', cb =>
+  pump(
+    [
+      gulp.src('src/assets/js/*.js'),
+      // sourcemaps.init(),
+      babel({
+        presets: ['env']
+      }),
+      uglify(),
+      rename({
+        suffix: '.min'
+      }),
+      // sourcemaps.write('.'),
+      gulp.dest('dist/assets/js'),
+      browserSync.stream()
+    ],
+    cb
+  )
 );
 
 // Browser Sync
 
-gulp.task("serve", ["sass", "minifyJs"], function() {
+gulp.task('serve', ['sass'], function() {
   browserSync.init({
     server: {
-      index: "index.html"
+      index: 'index.html'
     }
   });
-  gulp.watch("src/assets/style/sass/**/*.sass", ["sass"]);
-  gulp.watch("src/assets/js/*.js", ["minifyJs"]);
-  gulp.watch("*.html").on("change", browserSync.reload);
+  gulp.watch('src/assets/style/sass/**/*.sass', ['sass']);
+  gulp.watch('src/assets/js/*.js', ['minifyJs']);
+  gulp.watch('*.html').on('change', browserSync.reload);
 });
 
 // Default
-gulp.task("default", ["serve"]);
+gulp.task('default', ['serve']);
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,54 +1,53 @@
 // Show header text
 document.addEventListener('DOMContentLoaded', function() {
-	setTimeout(function() {
-		const headerText = document.querySelector('.camlab-story');
-		const courses = document.querySelector('.camlab-courses');
-		headerText.classList.remove('hidden-text');
-		headerText.classList.add('slideLeft');
-		courses.classList.remove('hidden-text');
-		courses.classList.add('slideRight');
-	}, 1000);
+  setTimeout(function() {
+    const headerText = document.querySelector('.camlab-story');
+    const courses = document.querySelector('.camlab-courses');
+    headerText.classList.remove('hidden-text');
+    headerText.classList.add('slideLeft');
+    courses.classList.remove('hidden-text');
+    courses.classList.add('slideRight');
+  }, 1000);
 });
 
 // Responsive navbar
 mobileNav();
 
-
 function mobileNav() {
-	const hamburger = document.querySelector('.hamburger-menu');
-	const nav = document.querySelector('.nav-content');
-	hamburger.addEventListener('click', function() {
-		hamburger.classList.toggle('animated');
-		nav.classList.toggle('display-none');
-		nav.classList.toggle('slideLeft');
-		closeNav();
-	});
+  const hamburger = document.querySelector('.hamburger-menu');
+  const nav = document.querySelector('.nav-content');
+  hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('animated');
+    nav.classList.toggle('display-none');
+    nav.classList.toggle('slideLeft');
+    closeNav();
+  });
 }
 
 function closeNav() {
-	const hamburger = document.querySelector('.hamburger-menu');
-	const nav = document.querySelector('.nav-content');
-	let links = document.querySelectorAll('.closeNavJs');
-	links = Array.from(links);
-	links.forEach(function(elem) {
-		elem.addEventListener('click', function() {
-			hamburger.classList.remove('animated');
-			nav.classList.remove('slideLeft');
-			nav.classList.add('display-none');
-		});
-	});
+  const hamburger = document.querySelector('.hamburger-menu');
+  const nav = document.querySelector('.nav-content');
+  let links = document.querySelectorAll('.closeNavJs');
+  links = Array.from(links);
+  links.forEach(function(elem) {
+    elem.addEventListener('click', function() {
+      hamburger.classList.remove('animated');
+      nav.classList.remove('slideLeft');
+      nav.classList.add('display-none');
+    });
+  });
 }
 
 // Dropdown menu
 openDrop();
 
 function openDrop() {
-	const drop = document.querySelector('.dropdown');
-	const content = document.querySelector('.content');
-	drop.addEventListener('click', function() {
-		content.classList.toggle('open');
-		content.classList.toggle('slideDown');
-	});
+  const drop = document.querySelector('.dropdown');
+  const content = document.querySelector('.content');
+  drop.addEventListener('click', function() {
+    content.classList.toggle('open');
+    content.classList.toggle('slideDown');
+  });
 }
 
 // Courses
@@ -56,14 +55,137 @@ function openDrop() {
 showInfo();
 
 function showInfo() {
-	let corsi = document.querySelectorAll('.getInfoJs');
-	corsi = Array.from(corsi);
-	let info = document.querySelectorAll('.showInfoJs');
-	info = Array.from(info);
-	corsi.forEach(function(el, index) {
-		el.addEventListener('click', function() {
-			info[index].classList.toggle('display-none');
-			info[index].classList.toggle('slideDown');
-		});
-	});
+  let corsi = document.querySelectorAll('.getInfoJs');
+  corsi = Array.from(corsi);
+  let info = document.querySelectorAll('.showInfoJs');
+  info = Array.from(info);
+  corsi.forEach(function(el, index) {
+    el.addEventListener('click', function() {
+      info[index].classList.toggle('display-none');
+      info[index].classList.toggle('slideDown');
+    });
+  });
+}
+
+// prenotazione corso wizard form
+// let currentTab = 0;
+// showTab(currentTab);
+
+// const showTab = n => {
+//   const tab = document.getElementsByClassName('tab');
+//   tab[n].style.display = 'block';
+
+//   if (n === 0) {
+//     document.querySelector('#prevBtn').style.display = 'none';
+//   } else {
+//     document.querySelector('#prevBtn').style.display = 'inline';
+//   }
+
+//   if (n === tab.length - 1) {
+//     document.querySelector('#nextBtn').innerHTML = 'Prenota';
+//   } else {
+//     document.querySelector('#nextBtn').innerHTML = 'Continua';
+//   }
+
+//   fixStepIndicator(n);
+// };
+
+// function nextPrev(n) {
+//   const tab = document.getElementsByClassName('tab');
+//   if (n === 1) return false;
+
+//   tab[currentTab].style.display = 'none';
+//   currentTab = currentTab + n;
+
+//   if (currentTab >= tab.length) {
+//     document.querySelector('#prenotazione-corso-form').submit;
+//     return false;
+//   }
+//   showTab(currentTab);
+// }
+
+// function fixStepIndicator(n) {
+//   let i,
+//     step = document.getElementsByClassName('step');
+//   for (i = 0; i < step.length; i++) {
+//     step[i].className = step[i].className.replace('active', '');
+//   }
+//   step[n].className += ' active';
+// }
+
+var currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the current tab
+
+function showTab(n) {
+  // This function will display the specified tab of the form ...
+  var x = document.getElementsByClassName('tab');
+  x[n].style.display = 'flex';
+  // ... and fix the Previous/Next buttons:
+  if (n == 0) {
+    document.getElementById('prevBtn').style.display = 'none';
+  } else {
+    document.getElementById('prevBtn').style.display = 'inline';
+  }
+  if (n == x.length - 1) {
+    document.getElementById('nextBtn').innerHTML = 'Prenota';
+  } else {
+    document.getElementById('nextBtn').innerHTML = 'Continua';
+  }
+  // ... and run a function that displays the correct step indicator:
+  fixStepIndicator(n);
+}
+
+function nextPrev(n) {
+  // This function will figure out which tab to display
+  var x = document.getElementsByClassName('tab');
+  // Exit the function if any field in the current tab is invalid:
+  if (n == 1 && !validateForm()) return false;
+  // Hide the current tab:
+  x[currentTab].style.display = 'none';
+  // Increase or decrease the current tab by 1:
+  currentTab = currentTab + n;
+  // if you have reached the end of the form... :
+  if (currentTab >= x.length) {
+    //...the form gets submitted:
+    document.getElementById('prenotazione-corso-form').submit();
+    return false;
+  }
+  // Otherwise, display the correct tab:
+  showTab(currentTab);
+}
+
+function validateForm() {
+  // This function deals with validation of the form fields
+  var x,
+    y,
+    i,
+    valid = true;
+  x = document.getElementsByClassName('tab');
+  y = x[currentTab].getElementsByTagName('input');
+  // A loop that checks every input field in the current tab:
+  for (i = 0; i < y.length; i++) {
+    // If a field is empty...
+    if (y[i].value == '') {
+      // add an "invalid" class to the field:
+      y[i].className += ' invalid';
+      // and set the current valid status to false:
+      valid = false;
+    }
+  }
+  // If the valid status is true, mark the step as finished and valid:
+  if (valid) {
+    document.getElementsByClassName('step')[currentTab].className += ' finish';
+  }
+  return valid; // return the valid status
+}
+
+function fixStepIndicator(n) {
+  // This function removes the "active" class of all steps...
+  var i,
+    x = document.getElementsByClassName('step');
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(' active', '');
+  }
+  //... and adds the "active" class to the current step:
+  x[n].className += ' active';
 }
